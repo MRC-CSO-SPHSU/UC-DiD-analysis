7fb7ec2becac4eb8fc5ae4d4d9b0f6b001bf4bbd
Andy Baxter <andy@andybaxter.me>
2022-11-18 16:00:12 +0000

    expanding metrics

--- R/collecting_metrics_for_models.R
+++ R/collecting_metrics_for_models.R
@@ -6,10 +6,12 @@ cl <- parallel::makePSOCKcluster(floor(0.98*cores))
 registerDoParallel(cl)
 
 
+cv_class_log <- fit_resamples(wf_class_log, mc_train_set, control = control_resamples(save_pred = TRUE,
+                                                                                      parallel_over = "everything",
+                                                                                      verbose = TRUE),
+                              metrics = metric_set(sens, spec, ppv, npv, roc_auc))
+
+
 cv_class_xg <- fit_resamples(recipe_class_xg, mc_train_set, control = control_resamples(save_pred = TRUE,
                                                                                         parallel_over = "everything",
                                                                                         verbose = TRUE))
-
-cv_class_log <- fit_resamples(wf_class_log, mc_train_set, control = control_resamples(save_pred = TRUE,
-                                                                                      parallel_over = "everything",
-                                                                                      verbose = TRUE))
