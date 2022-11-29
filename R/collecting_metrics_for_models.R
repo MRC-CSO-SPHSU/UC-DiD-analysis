26cb07aa9865405f686f367c93cb698bd7a8060f
Andy Baxter <andrewbaxter439@gmail.com>
2022-11-29 09:06:43 +0000

    tidied collection script

--- R/collecting_metrics_for_models.R
+++ R/collecting_metrics_for_models.R
@@ -15,7 +15,7 @@ cv_class_log <- fit_resamples(wf_class_log, mc_train_set, control = control_resa
 cv_class_xg <- fit_resamples(recipe_class_xg, mc_train_set, control = control_resamples(save_pred = TRUE,
                                                                                         parallel_over = "everything",
                                                                                         verbose = TRUE),
-                              metrics = metric_set(sens, spec, ppv, npv, roc_auc))
+                             metrics = metric_set(sens, spec, ppv, npv, roc_auc))
 
 stopCluster(cl)
 
@@ -25,8 +25,6 @@ cv_class_log |>
 
 cv_class_xg |> 
   collect_metrics()
-
-stopCluster(cl)
 
 
 cv_class_log
